933a5549e22e13af75a45a9646af0ca1bc197c7c
unknown <nilimawadal1@gmail.com>
2021-04-16 15:06:00 +0530

    [Nilima] Added File for Check for Option

--- SnakeLadderGame/SnakeLadderGame/Program.cs
+++ SnakeLadderGame/SnakeLadderGame/Program.cs
@@ -11,16 +11,32 @@ namespace SnakeLadderGame
         static void Main(string[] args)
         {
             /// <summary>
-            /// Rolls the dice.
+            /// Creating CheckFor Option Method
             /// </summary>
-            
-                Console.WriteLine("WelCome To Snake And Ladder");
-                int position = 0;//Initialize Position
+           
+                int position = 0;
                 string player = "Player1";
-                Random random = new Random();//generating random number
-                int Dice = random.Next(1, 7);//Store random value in Dice
-                Console.WriteLine(Dice);//Print Dice 
-            }
+                Random random = new Random();//Generating Random Number
+                int Dice = random.Next(1, 7);//It will create 1,2,3,4,5,6 Random Number and store in Dice
+                Console.WriteLine("value on the Dice" + Dice);
+                Random random1 = new Random();
+                int check = random1.Next(0, 3);//Creating Random Number for Check Option
+                //Starting iF Condition
+                if (check == 0)
+                {
+                    Console.WriteLine("Their is No Play");
+                }
+                else if (check == 1)
+                {
+                    Console.WriteLine("You get Ladder");
+                    position = position + Dice;
+                }
+                else if (check == 2)
+                {
+                    Console.WriteLine("You get Snake");
+                    position = position - Dice;//End of If Condition
+                }
         }
     }
+}
 
