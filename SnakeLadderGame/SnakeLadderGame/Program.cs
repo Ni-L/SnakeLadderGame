19e59c081476d60c39d2b9e0ababd0f51192bae1
unknown <nilimawadal1@gmail.com>
2021-04-16 14:50:56 +0530

    [Nilima] Added Stating point of Snake and Ladder

--- SnakeLadderGame/SnakeLadderGame/Program.cs
+++ SnakeLadderGame/SnakeLadderGame/Program.cs
@@ -4,9 +4,19 @@ namespace SnakeLadderGame
 {
     class Program
     {
+        /// <summary>
+        /// Creating Program for Position =0
+        /// </summary>
+        
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome to The Snake And Ladder Game");
+                int position = 0;//Initialize position =0
+                string player = "Player1";
+                Console.WriteLine("Position" + position);//print Position
+            
         }
     }
+
 }
+   
